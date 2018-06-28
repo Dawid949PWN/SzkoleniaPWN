e15ac7ce8f286786496c85ae348f7de3a06d693d
Dawid <dmago808@gmail.com>
2018-06-28 16:34:22 +0200

    py

---
+++ ćwiczeniapydaw2.py
@@ -0,0 +1,12 @@
+
+liczba1 = None
+napis = input("Podaj liczbe1 (%): ")
+liczba1 = int(napis)
+
+liczba2 = None
+napis = input("Podaj liczbe2 (%): ")
+liczba2 = int(napis)
+wynik = (liczba1 / 100 * liczba2)
+print("Wyszukane wynik: ", wynik)
+
+
