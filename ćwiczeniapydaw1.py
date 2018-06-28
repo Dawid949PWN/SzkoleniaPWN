e597cb77d00cc39f89476b8afdee8ed3ead6c63c
Dawid <dmago808@gmail.com>
2018-06-28 16:32:15 +0200

    py

---
+++ ćwiczeniapydaw1.py
@@ -0,0 +1,9 @@
+imie = "Arkadiusz"
+nazwisko = "Domalski"
+wiek = 22
+stanowisko = "młodszy developer procesu"
+a = str(imie + " " + nazwisko)
+b = str(wiek) + " " + str(stanowisko)
+napis= a + " " + b + "\n"
+print(napis * 6)
+
