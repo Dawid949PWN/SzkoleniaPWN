8ddb2cb7d3cd12f1c5f132e73af1c207f325d5ce
Dawid <dmago808@gmail.com>
2018-07-16 22:23:41 +0200

    java123579

---
+++ j1/j7.java
@@ -0,0 +1,13 @@
+package j1;
+import java.math.BigInteger;
+
+public class j7 {
+        public static void main(String[] args) {
+
+            BigInteger a = new BigInteger("4242424242424244242");
+            BigInteger b = new BigInteger("7465647465647566475");
+
+            System.out.println(a);
+            System.out.println(b);
+        }
+    }
---
+++ j1/j9.java
@@ -0,0 +1,13 @@
+package j1;
+import java.util.Scanner;
+
+public class j9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        StringBuffer stringBuffer2 = new StringBuffer();
+        stringBuffer2.append("text");
+        System.out.println(stringBuffer2.reverse());
+    }
+}
