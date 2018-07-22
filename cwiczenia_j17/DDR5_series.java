a980d0b2035bda94b86beb2f6d8277ae4df56514
Dawid <dmago808@gmail.com>
2018-07-22 23:55:21 +0200

    java1717

---
+++ cwiczenia_j17/DDR5_series.java
@@ -0,0 +1,18 @@
+package cwiczenia_j17;
+
+public class DDR5_series {
+    public static void main(String[] args) {
+
+        kartygraficzne kartygraficzne = new kartygraficzne();
+        kartygraficzne kartygraficzne1 = new kartygraficzne();
+
+        System.out.println("Przykładzie dwie karty graficzne rodzaje: ");
+        System.out.println("-------------------- \n");
+
+        kartygraficzne1.setVideocardname("\n" + "ATI" + "\t" + "Radeon ");
+
+        System.out.println(kartygraficzne.getVideocardname());
+        System.out.println(kartygraficzne1.getVideocardname());
+    }
+
+}
---
+++ cwiczenia_j17/kartygraficzne.java
@@ -0,0 +1,16 @@
+package cwiczenia_j17;
+
+public class kartygraficzne {
+
+    private String videocardname = ("Nvidia\t" + "Geforce");
+
+
+    public String getVideocardname() {
+        return this.videocardname;
+    }
+
+    public void setVideocardname(String videocardname1) {
+        this.videocardname = videocardname1;
+    }
+
+}
