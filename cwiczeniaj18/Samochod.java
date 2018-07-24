487477c908cb613e1abfd846f2f297922e6e77c5
Dawid <dmago808@gmail.com>
2018-07-25 01:05:53 +0200

    java1818

---
+++ cwiczeniaj18/Samochod.java
@@ -0,0 +1,46 @@
+package cwiczeniaj18;
+
+public class Samochod {
+
+    private String marka;
+    private int rocznik;
+    private String typpaliwy;
+    private int paliwo;
+
+    // Marka auta
+    public void writeName() {
+        System.out.println("Samochod marka " + this.marka);
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public int getRocznik() {
+        return rocznik;
+    }
+
+    public void setRocznik(int rocznik) {
+        this.rocznik = rocznik;
+    }
+
+    public String getTyppaliwy() {
+        return typpaliwy;
+    }
+
+    public void setTyppaliwy(String typpaliwy) {
+        this.typpaliwy = typpaliwy;
+    }
+
+    public int getPaliwo() {
+        return paliwo;
+    }
+
+    public void setPaliwo(int paliwo) {
+        this.paliwo = paliwo;
+    }
+}
