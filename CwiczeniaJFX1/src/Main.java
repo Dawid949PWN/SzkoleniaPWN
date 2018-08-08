5b85689962dd95ae254d161d2525ad2d640b34bb
magodaw486 <40119829+magodaw486@users.noreply.github.com>
2018-08-08 14:15:54 +0200

    java_cwiczelog...

---
+++ CwiczeniaJFX1/src/Main.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void init(){
+        System.out.println("metod initialize");
+    }
+
+    @Override
+
+    public void start(Stage primaryStage) throws Exception{
+        System.out.println("metod start");
+        Parent root = FXMLLoader.load(getClass().getResource("new/loginnew.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public void stop(){
+        System.out.println("metod stop");
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
